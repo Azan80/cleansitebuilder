8cc379a3a17a35686ac31f0106b2b809a8d963ab
Azan80 <azanabid8888@gmail.com>
2025-12-16 16:19:24 +0500

    dev changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,23 @@ export default function RootLayout({
             })
           }}
         />
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17809019638"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17809019638');
+            `,
+          }}
+        />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
